d0299c0013d43dad58aa9471b1bf1d19c02e16d7
Muzammil <jemniekk@gmail.com>
2024-05-22 15:45:14 +0500

    first commit

---
+++ next-js-contact-form-rest-api.php
@@ -0,0 +1,32 @@
+<?php
+/**
+
+ * Plugin Name:       Next js Contact form Rest-API plugin
+
+ * Plugin URI:        https://noderavel.com/
+
+ * Description:       Accepts Next js Contact form Submissions
+
+ * Version:           1.0.0
+
+ * Requires at least: 5.5
+
+ * Requires PHP:      7.2
+
+ * Author:            Muzammil Ahmed
+
+ * Author URI:        https://noderavel.com/
+
+ * License:           GPL v2 or later
+
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+
+ */
+
+
+
+ // Direct access protection
+
+ defined('ABSPATH') or die();
+
+
